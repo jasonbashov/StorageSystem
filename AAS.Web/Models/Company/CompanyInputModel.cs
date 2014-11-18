b18bc758009ac930588b3df2bd6e03e738976bec
jasonbashov <jbashov@gmail.com>
2014-11-18 09:35:32 +0200

    Handled Html tags and string template
    And some refactoring

--- AAS.Web/Models/Company/CompanyInputModel.cs
+++ AAS.Web/Models/Company/CompanyInputModel.cs
@@ -1,22 +1,34 @@
 ﻿namespace AAS.Web.Models
 {
     using System.ComponentModel.DataAnnotations;
+    using System.Web.Mvc;
 
     public class CompanyInputModel
     {
         [Required]
+        [UIHint("TextEditor")]
+        [Display(Name = "Name")]
+        [AllowHtml]
         public string Name { get; set; }
         
         [Required]
+        [UIHint("TextEditor")]
+        [Display(Name = "Adress")]
         public string Adress { get; set; }
         
         [Required]
         [StringLength(11, MinimumLength = 11)]
+        [UIHint("TextEditor")]
+        [Display(Name = "Bulstat")]
         public string Bulstat { get; set; }
 
         [Required]
+        [UIHint("TextEditor")]
+        [Display(Name = "Accountable Person")]
         public string AccountablePerson { get; set; }
-
+        
+        [UIHint("TextEditor")]
+        [Display(Name = "Image Url")]
         public string ImgUrl { get; set; }
 
     }
